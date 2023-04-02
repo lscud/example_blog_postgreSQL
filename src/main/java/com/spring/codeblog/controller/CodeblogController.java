41bc8ef13c553af5af2528d9110b15edd6f444a6
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-04-02 10:52:28 -0300

    Starting construct page with BootStrap

---
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -0,0 +1,26 @@
+package com.spring.codeblog.controller;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CodeblogController {
+
+    @Autowired
+    private CodeblogService codeblogService;
+
+    @GetMapping(value = "/posts")
+    public ModelAndView getPosts(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+
+    }
+}
