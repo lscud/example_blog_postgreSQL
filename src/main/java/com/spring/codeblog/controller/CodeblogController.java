17e8e2a64b3003a6ccb168c490bf0df677edf0f0
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-04-03 16:23:58 -0300

    adding a new post

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,10 +4,16 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,6 +37,22 @@ public class CodeblogController {
         Post post = codeblogService.findById(id);
         mv.addObject("post", post);  //quando for acessar a view la no html para renderizar, vou buscar essa palavra posts (atributo), porque dentro dessa palavra estarão meus posts;
         return mv;
+
+    }
+
+    @GetMapping(value = "/newpost")
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @PostMapping(value = "/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
 
     }
 }
