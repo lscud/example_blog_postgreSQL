ba5ec9e5903e129dabe45ae3aad1346ca4ce59ed
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-04-03 09:17:29 -0300

    find post with id method

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,15 @@ public class CodeblogController {
         ModelAndView mv = new ModelAndView("posts"); //MVN ->model view anda controller. model -> Post, view -> pagina html, controller -> CodeblogController
         List<Post> posts = codeblogService.findAll();
         mv.addObject("list_posts", posts);  //quando for acessar a view la no html para renderizar, vou buscar essa palavra posts (atributo), porque dentro dessa palavra estarão meus posts;
+        return mv;
+
+    }
+
+    @GetMapping(value = "/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable("id") Long id){
+        ModelAndView mv = new ModelAndView("postDetail"); //MVN ->model view anda controller. model -> Post, view -> pagina html, controller -> CodeblogController
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);  //quando for acessar a view la no html para renderizar, vou buscar essa palavra posts (atributo), porque dentro dessa palavra estarão meus posts;
         return mv;
 
     }
